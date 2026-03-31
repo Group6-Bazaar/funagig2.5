97361db9c1f9b84216ca1091b6dd48b3ce1d159b
Nickson-twinomugisha <twinnickson16@gmail.com>
2026-03-31 16:55:44 +0300

    feat: replace heart button with audio messaging placeholder in business and student chat interfaces

--- src/pages/business/Messaging.jsx
+++ src/pages/business/Messaging.jsx
@@ -384,10 +384,10 @@ const Messaging = () => {
                                             </svg>
                                             <input type="file" accept="image/*" style={{ display: 'none' }} onChange={handleFileChange} />
                                         </label>
-                                        {/* Like heart */}
-                                        <button className="ig-icon-btn" onClick={() => { setMessageInput('❤️'); }} title="Send heart">
+                                        {/* Audio */}
+                                        <button className="ig-icon-btn" onClick={() => { toast.info('Audio messaging coming soon!'); }} title="Send audio">
                                             <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
-                                                <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
                                             </svg>
                                         </button>
                                     </>
